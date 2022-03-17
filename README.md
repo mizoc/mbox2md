c230013c8352fb02682bf0a8ff4dc6ac60daadb2
mizoc <mizoc@ymail.ne.jp>
2022-03-17 17:21:26 +0900

    :sparkles: It worked! TODO:Attachments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mbox2txt
 Convert mbox file to txt
+
+`$python3 ./convert.py IN.mbox >OUT.txt`
--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 from email.header import decode_header
 import re
 from pytz import timezone
@@ -8,15 +9,15 @@ import mailbox
 TIMEZONE = "Asia/Tokyo"
 
 
-def read_contents(filepath):
+def main(filepath):
     mbox = mailbox.mbox(filepath)
     keys = mbox.keys()
     for key in keys:
         print(
             f"""
 
-        -------------------
-        {key}"""
+#######################################
+No.{key}"""
         )
 
         mail_data = mbox.get(key)
@@ -26,14 +27,23 @@ def read_contents(filepath):
         print(parser.parse(mail_date).astimezone(timezone(TIMEZONE)).strftime("Date(%Z):%Y/%b/%d (%a) %H:%M:%S"))
 
         # subject
-        header = decode_header(mail_data["Subject"])
-        print(f"Subject:{header[0][0].decode(header[0][1], 'ignore')}")
+        # print(mail_data)
+        if mail_data["Subject"] is None:
+            print("Subject:None")
+        else:
+            header = decode_header(mail_data["Subject"])
+            # if type(header[0][0]) == str:
+            if header[0][1] is None:
+                print(f"Subject:{header[0][0]}")
+            else:
+                print(f"Subject:{header[0][0].decode(header[0][1], 'ignore')}")
 
         # mail addr
         try:
             print(f"From:{re.findall(' <(.*)>', mail_data['From'])[0]}")
         except:
-            print(mail_data["from"])
+            print(f"From:{mail_data['from']}")
+        print("#######################################")
 
         i = 0
         for aa_msg in mail_data.walk():
@@ -52,24 +62,21 @@ def read_contents(filepath):
                         print("** Cannot decode.Cannot specify charset ***" + msg.get("From"))
                 print(a_text)
 
-            # image base64だろ
+            # image どうせbase64だろ
             elif "image" in aa_msg.get_content_type():
                 import base64
                 import cv2
                 import numpy as np
 
                 cv2.imwrite(
-                    f"./attachments/0-{str(i)}.jpg",
+                    f"./attachments/{key}-{str(i)}.jpg",
                     cv2.imdecode(np.frombuffer(base64.b64decode(aa_msg.get_payload()), dtype=np.uint8), cv2.IMREAD_COLOR),
                 )
-                print(f"Attachment:{aa_msg.get_filename()}(./attachments/0-{i})")
+                print(f"Attachment:{aa_msg.get_filename()}(./attachments/{key}-{i})")
 
                 i += 1
 
 
-def main():
-    read_contents("./20160233.mbox")
-
-
 if __name__ == "__main__":
-    main()
+    # 引数でfile path指定する
+    main(sys.argv[1])
